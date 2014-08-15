6b11e5dc6b82cb7268d407df06f61fb78e7e96a6
kamol <kamolcu@gmail.com>
2014-08-15 19:52:55 +0700

    Refactor code

--- real_updator.bat
+++ real_updator.bat
@@ -8,22 +8,21 @@ REM SET PATH=%APP%;%SCRIPT%;%GIT_ENV%;%PATH%
 SET CURRENT_PATH=%~dp0
 SET CURRENT_DRIVE=%CURRENT_PATH:~0,2%
 
-"%APP%\python.exe" -m compileall
+
 REM ===== Update project-cylon
 "%APP%\python.exe" "%~dp0..\App\Lib\site-packages\easy_install.py" pip
-REM "%SCRIPT%\pip.exe" uninstall six -y
 "%APP%\python.exe" "%~dp0..\App\Lib\site-packages\easy_install.py" six
 "%SCRIPT%\pip.exe" uninstall project-cylon -y
 "%SCRIPT%\pip.exe" uninstall behave -y
 "%SCRIPT%\pip.exe" uninstall pyyaml -y
 "%SCRIPT%\pip.exe" uninstall selenium -y
 "%SCRIPT%\pip.exe" uninstall colorama -y
-REM "%SCRIPT%\pip.exe" install --upgrade six
 "%SCRIPT%\pip.exe" install --upgrade selenium
 "%SCRIPT%\pip.exe" install --upgrade colorama
 "%SCRIPT%\pip.exe" install --upgrade pyyaml
 "%SCRIPT%\pip.exe" install --upgrade behave
 "%SCRIPT%\pip.exe" install --upgrade project-cylon
+"%APP%\python.exe" -m compileall
 REM =====
 
 %CURRENT_DRIVE%
