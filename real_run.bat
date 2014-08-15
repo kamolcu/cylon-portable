0051ddf1da0bf6cefc5d4d8b3003253c2891089f
kamol <kamolcu@gmail.com>
2014-08-15 16:08:54 +0700

    Add file real_run.bat

---
+++ real_run.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+
+SET SCRIPT=%~dp0..\App\Scripts
+SET GIT_ENV=%~dp0..\App\GitPortable\App\Git\bin
+SET PATH=%SCRIPT%;%GIT_ENV%;%PATH%
+SET CURRENT_PATH=%~dp0
+SET CURRENT_DRIVE=%CURRENT_PATH:~0,2%
+
+%CURRENT_DRIVE%
+cd "%~dp0..\sanity"
+cd itruemart-sanity
+
+cylon run all
