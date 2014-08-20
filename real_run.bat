df11a37843cda28aab12f78e7a652cb6b8929ca2
kamol <kamolcu@gmail.com>
2014-08-20 16:08:05 +0700

    Update to run iTM and WLS together

--- real_run.bat
+++ real_run.bat
@@ -8,8 +8,13 @@ SET CURRENT_PATH=%~dp0
 SET CURRENT_DRIVE=%CURRENT_PATH:~0,2%
 
 %CURRENT_DRIVE%
+ECHO WELOVE_SANITY ...
+cd "%~dp0..\sanity"
+cd projectwls-sanity
+"%~dp0..\App\Scripts\cylon.exe" run all
+
+ECHO ITRUEMART SANITY ...
 cd "%~dp0..\sanity"
 cd itruemart-sanity
-
 "%~dp0..\App\Scripts\cylon.exe" run all
 pause
